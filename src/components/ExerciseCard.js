aacc157e8a00a3e36be1281dbb91bc770cd6c12e
Matheus Ghignatti <matheusghignatti@gmail.com>
2022-11-06 20:51:51 -0300

    add exercises part 2 and exerciseCard

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -2,8 +2,49 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Button, Stack, Typography } from "@mui/material";
 
-const ExerciseCard = () => {
-  return <div>ExerciseCard</div>;
+const ExerciseCard = ({ exercise }) => {
+  return (
+    <Link className="exercise-card" to={`/exercise/${exercise.id}`}>
+      <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
+      <Stack direction="row">
+        <Button
+          sx={{
+            background: "#ffa9a9",
+            borderRadius: "20px",
+            color: "#fff",
+            fontSize: "14px",
+            ml: "21px",
+            textTransform: "capitalize",
+          }}
+        >
+          {exercise.bodyPart}
+        </Button>
+        <Button
+          sx={{
+            borderRadius: "20px",
+            background: "#fcc757",
+            color: "#fff",
+            fontSize: "14px",
+            ml: "21px",
+            textTransform: "capitalize",
+          }}
+        >
+          {exercise.target}
+        </Button>
+      </Stack>
+      <Typography
+        color="#000"
+        fontWeight="bold"
+        fontSize="22px"
+        ml="21px"
+        mt="11px"
+        pb="10px"
+        textTransform="capitalize"
+      >
+        {exercise.name}
+      </Typography>
+    </Link>
+  );
 };
 
 export default ExerciseCard;
