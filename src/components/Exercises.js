0d64489dc6b41e0de04b531118f512b656d74973
Matheus Ghignatti <matheusghignatti@gmail.com>
2022-11-21 03:48:34 -0300

    api update

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -25,18 +25,22 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
   useEffect(() => {
     const fetchExercisesData = async () => {
       let exercisesData = [];
-      bodyPart === "all"
-        ? (exercisesData = await fetchData(
-            "https://exercisedb.p.rapidapi.com/exercises",
-            exerciseOptions
-          ))
-        : (exercisesData = await fetchData(
-            `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`,
-            exerciseOptions
-          ));
+
+      if (bodyPart === "all") {
+        exercisesData = await fetchData(
+          "https://exercisedb.p.rapidapi.com/exercises",
+          exerciseOptions
+        );
+      } else {
+        exercisesData = await fetchData(
+          `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`,
+          exerciseOptions
+        );
+      }
 
       setExercises(exercisesData);
     };
+
     fetchExercisesData();
   }, [bodyPart]);
 
